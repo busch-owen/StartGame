c3d05d11cee3594baaae2670eb88d7dde2bac5dc
busch-owen <buschowen@gmail.com>
2024-07-27 22:34:04 -0600

    Bug fix

--- StartGame/Assets/Scripts/UI/MainMenu.cs
+++ StartGame/Assets/Scripts/UI/MainMenu.cs
@@ -18,7 +18,7 @@ public class MainMenu : MonoBehaviour
 
     public void StartGame()
     {
-        SceneManager.LoadScene(scene);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void LoadMemeMenuUI()
