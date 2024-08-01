6f5b2d2f8c3d3d0868fe1083a7803ef3a2fd4fbb
busch-owen <buschowen@gmail.com>
2024-08-01 15:30:49 -0600

    FINAL touches

--- StartGame/Assets/Scripts/ScriptableObjects/MapRollerSO.cs
+++ StartGame/Assets/Scripts/ScriptableObjects/MapRollerSO.cs
@@ -1,8 +1,20 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu(menuName = "GameStats/MapRoller", fileName = "Map Roller Stats")]
 public class MapRollerSO : ScriptableObject
 {
     [field: SerializeField] public float RotationSpeed { get; private set; }
     [field: SerializeField] public float ReturnSpeed  { get; private set; }
+
+    private Slider sensSldier;
+    
+    public void ChangeSensitivity()
+    {
+        sensSldier = FindObjectOfType<Slider>();
+        TMP_Text sensText = sensSldier.GetComponentInChildren<TMP_Text>();
+        RotationSpeed = sensSldier.value;
+        sensText.text = $"Sensitivity: {RotationSpeed}";
+    }
 }
